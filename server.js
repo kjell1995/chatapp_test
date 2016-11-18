0a3794fe9d54702bb897d53caa98222c86ef205f
kjell1995 <kjelldeboysere@hotmail.com>
2016-11-18 21:12:55 +0100

    working caht server with nickname functionality

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const app= express();
-const pug = require('pug');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
-app.set("view options", {layout: false});
+app.set("port", process.env.PORT || 2000);
 app.use(express.static(__dirname + "/public"));
 
 io.sockets.on('connection', (socket)=>{
   console.log('a new user connected');
-  socket.on('setPseudo', (data)=>{
-    socket.set('pseudo', data);
+
+  socket.on('nickname', (data)=>{
+    socket.nickname = data;
   });
-  socket.on('message', (message)=> {
-    socket.get('pseudo', function (error, name) {
-        const data = { 'message' : message, pseudo : name };
-        socket.broadcast.emit('message', data);
-        console.log("user " + name + " send this : " + message);
-    });
+  socket.on('chat message', (message)=> {
+    const newMsg = {nick: socket.nickname, msg: message}
+    io.emit('chat message',newMsg);
 });
+socket.on('disconnect',()=>{
+    console.log(socket.nickname + ' disconnected');
+    io.emit('disconnection', socket.nickname);
 });
+
+});
+
+
+
+
 app.get('/', (req,res)=>{
-  res.render('home.pug');
+  res.sendFile(__dirname +'/home.html');
 });
 
 
-app.listen(3000);
+http.listen(app.get("port"), () => {
+  console.log("Server started on http://localhost: " + app.get("port"));
+});
