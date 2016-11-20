4adee10b98e69cafd5fb1a8df8d56a1906f20bac
kjell1995 <kjelldeboysere@hotmail.com>
2016-11-20 19:52:07 +0100

    chat works with user count, disconnect messages

--- server.js
+++ server.js
@@ -5,26 +5,27 @@ const io = require('socket.io')(http);
 
 app.set("port", process.env.PORT || 2000);
 app.use(express.static(__dirname + "/public"));
-
+var users=0;
 io.sockets.on('connection', (socket)=>{
   console.log('a new user connected');
+  users++;
+  io.emit('NrOfUsrs',users);
 
   socket.on('nickname', (data)=>{
     socket.nickname = data;
   });
   socket.on('chat message', (message)=> {
-    const newMsg = {nick: socket.nickname, msg: message}
+    const newMsg = {nick: socket.nickname, msg: message};
     io.emit('chat message',newMsg);
-});
-socket.on('disconnect',()=>{
+  });
+  socket.on('disconnect',()=>{
     console.log(socket.nickname + ' disconnected');
+    users--;
     io.emit('disconnection', socket.nickname);
-});
+    io.emit('NrOfUsrs',users);
+  });
 
 });
-
-
-
 
 app.get('/', (req,res)=>{
   res.sendFile(__dirname +'/home.html');
